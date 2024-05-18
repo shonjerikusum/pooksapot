e942cafc8cc07fa8f705e1d5fe47bd925d12a1d1
shonjerikusum <samseanjericho@gmail.com>
2024-05-18 23:37:34 +0800

    Add files via upload

---
+++ Finals CGP; SASS/JavaScript/functions.js
@@ -0,0 +1,17 @@
+function scrollToHome() {
+    document.getElementById('bahay').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToAbout() {
+    document.getElementById('tungkol').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToSkills() {
+    document.getElementById('karanasan').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToContacts() {
+    document.getElementById('tawag').scrollIntoView({ behavior: 'smooth' });
+}
+
+
